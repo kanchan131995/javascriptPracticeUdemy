4e13bfd57c7de5c3fd8a2aac8b2f5353ea14effe
kanchan131995 <43952777+kanchan131995@users.noreply.github.com>
2019-01-26 15:26:29 +0530

    all section added

---
+++ section 2-(javascript Essential)/excercise prompt/excercise prompt.js
@@ -0,0 +1,3 @@
+var yourName=prompt("what is your name");
+document.write("welcome "+yourName);
+alert("welcome");
---
+++ section 2-(javascript Essential)/excerciseArthmetic/excerciseArthmetic.js
@@ -0,0 +1,5 @@
+var number1=prompt("enter number1");
+var number2=prompt("enter number2");
+var result1=number1-number2;
+var result=Number(number1)+Number(number2);
+alert(result);
